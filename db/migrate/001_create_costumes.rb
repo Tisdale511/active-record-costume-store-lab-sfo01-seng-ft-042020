785ad43a5f3913cce1a478c000dbcd67c5847bd7
Tisdale511 <tisdalefry@gmail.com>
2020-05-02 18:56:55 -0700

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,3 +9,15 @@
 # value types according to the spec
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class CreateCostumes < ActiveRecord::Migration[6.0]
+    def change
+        create_table :costumes do |costume|
+            costume.string :name
+            costume.float :price
+            costume.string :image_url
+            costume.string :size
+            costume.timestamps
+        end
+    end
+end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,19 @@
 # Create your costume_stores migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class CreateCostumeStores < ActiveRecord::Migration[6.0]
+    def change
+        create_table :costume_stores do |stores|
+            stores.string :name
+            stores.string :location
+            stores.integer :costume_inventory
+            stores.integer :num_of_employees
+            stores.boolean :still_in_business
+            stores.datetime :opening_time
+            stores.datetime :closing_time
+        end
+    end
+
+end
+
